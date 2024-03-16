9c6b8e5fdf8ca17778c4c6d5157824feff6c8c35
Carlos Pires <extraterra1@gmail.com>
2024-03-16 12:10:53 +0000

    chore: enable cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const apiRouter = require('./routes/apiRouter');
 const handle404 = require('./middleware/handle404');
@@ -11,11 +11,11 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.json({ message: 'Welcome! You can access the API on /api' }));
 app.use('/api', apiRouter);
