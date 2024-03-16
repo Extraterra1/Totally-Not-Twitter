ef74849269dbd1e9f9f55032e4a2d6dcfa344ee8
Carlos Pires <extraterra1@gmail.com>
2024-03-16 12:01:46 +0000

    dev: api router

---
+++ backend/routes/apiRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.json({ message: 'Welcome to the TTN API!' });
+});
+
+module.exports = router;
