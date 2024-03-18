f53a25bb58357be5d5f4afbccf816b446385e201
Carlos Pires <extraterra1@gmail.com>
2024-03-18 12:09:52 +0000

    feat: tweet model

---
+++ backend/models/tweetModel.js
@@ -0,0 +1,35 @@
+const { default: mongoose, Schema } = require('mongoose');
+
+const tweetSchema = new Schema(
+  {
+    content: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    imgUrl: {
+      type: String
+    },
+    likes: [{ type: mongoose.Types.ObjectId, required: true, ref: 'User' }],
+    tweetType: {
+      type: String,
+      enum: ['tweet', 'retweet', 'reply'],
+      default: 'tweet'
+    },
+    replyTo: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Tweet',
+      default: null
+    },
+    retweetedTweet: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Tweet',
+      default: null
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Tweet', tweetSchema);
