97169f4eda54078125180cef0873cb4a601d7a6e
Carlos Pires <extraterra1@gmail.com>
2024-03-19 15:01:05 +0000

    fix: register route

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const authController = require('../controllers/authController');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.json({ message: 'Welcome to the TTN API!' });
 });
+
+// AUTH
+router.post('/register', authController.registerPOST);
 
 module.exports = router;
