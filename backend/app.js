564f4ffc4f99442fe579278e15ebecc040f7f4a6
Carlos Pires <extraterra1@gmail.com>
2024-03-19 14:54:39 +0000

    feat: passport auth

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ const apiRouter = require('./routes/apiRouter');
 const handle404 = require('./middleware/handle404');
 const errorHandler = require('./middleware/errorHandler');
 
+const passport = require('passport');
+const { strategy } = require('./config/passport');
+
 require('dotenv').config();
 
 const app = express();
@@ -16,6 +19,12 @@ const app = express();
 mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
+
+// Use JWT Strategy
+passport.use(strategy);
+
+// This will initialize the passport object on every request
+app.use(passport.initialize());
 
 app.use(cors());
 app.use(logger('dev'));
