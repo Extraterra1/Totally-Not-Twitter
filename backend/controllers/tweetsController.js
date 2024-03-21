8a8b3621a591f9c76fd775d90a6afe74b4f272af
Carlos Pires <extraterra1@gmail.com>
2024-03-21 11:29:21 +0000

    wip: tweet body validation

--- backend/controllers/tweetsController.js
+++ backend/controllers/tweetsController.js
@@ -1,1 +1,15 @@
-exports.createTweet = [];
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/userModel');
+const Tweet = require('../models/tweetModel');
+
+exports.createTweet = [
+  body('content', 'Invalid Content Type').optional().isString(),
+  body('tweetType', 'Invalid Tweet Type').optional().isString().isIn(['tweet', 'retweet', 'reply']),
+  body('replyTo')
+    .optional()
+    .isMongoId()
+    .withMessage('Invalid Tweet ID')
+    .custom(async (val) => await Tweet.findById(val))
+    .withMessage('Tweet not Found')
+];
