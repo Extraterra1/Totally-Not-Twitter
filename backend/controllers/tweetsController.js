8f95ea003386d923e8fac3202d8836279ddb6a8b
Carlos Pires <extraterra1@gmail.com>
2024-03-21 11:42:42 +0000

    feat: tweet body validation

--- backend/controllers/tweetsController.js
+++ backend/controllers/tweetsController.js
@@ -4,12 +4,25 @@ const User = require('../models/userModel');
 const Tweet = require('../models/tweetModel');
 
 exports.createTweet = [
-  body('content', 'Invalid Content Type').optional().isString(),
+  body('content', 'Invalid Content Type')
+    .optional()
+    .isString()
+    .if(body('tweetType').isIn(['tweet', 'reply']))
+    .notEmpty()
+    .withMessage('Tweet cannot be empty'),
   body('tweetType', 'Invalid Tweet Type').optional().isString().isIn(['tweet', 'retweet', 'reply']),
   body('replyTo')
     .optional()
     .isMongoId()
     .withMessage('Invalid Tweet ID')
-    .custom(async (val) => await Tweet.findById(val))
-    .withMessage('Tweet not Found')
+    .if(body('tweetType').equals('reply'))
+    .notEmpty()
+    .withMessage('Reply ID cannot be empty'),
+  body('retweetedTweet')
+    .optional()
+    .isMongoId()
+    .withMessage('Invalid Tweet ID')
+    .if(body('tweetType').equals('retweet'))
+    .notEmpty()
+    .withMessage('Retweet ID cannot be empty')
 ];
