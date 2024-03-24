730b14e076e9a62484d82cd2419c78e06c08a417
Carlos Pires <extraterra1@gmail.com>
2024-03-24 12:04:42 +0000

    feat: validate if user exists

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,8 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const isValidObjectID = require('mongoose').Types.ObjectId.isValid;
+const User = require('../models/userModel');
 
-exports.followUser = asyncHandler((req, res) => {
+exports.followUser = asyncHandler(async (req, res) => {
   const userID = req.params.id;
 
   if (!isValidObjectID(userID)) return res.status(400).json({ err: 'Invalid User ID' });
+
+  const user = await User.findById(userID);
+  if (!user) return res.status(404).json({ err: 'User not found' });
 });
