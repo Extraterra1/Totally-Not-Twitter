e2bab45c3f5422868ba93e1a860334fd362c3f83
Carlos Pires <extraterra1@gmail.com>
2024-03-27 11:27:49 +0000

    feat: fetch user tweets

--- backend/controllers/tweetsController.js
+++ backend/controllers/tweetsController.js
@@ -93,4 +93,8 @@ exports.getTweetsByUser = asyncHandler(async (req, res) => {
 
   const user = await User.findById(req.params.id);
   if (!user) return res.status(404).json({ err: 'User not found' });
+
+  const tweets = await Tweet.find({ author: req.params.id }).populate('author replyTo retweetedTweet');
+
+  return res.json({ tweets, count: tweets.length });
 });
