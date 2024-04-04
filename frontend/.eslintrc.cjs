c32d40280af6b17418e0e4b0c944cbd3a49910b8
Carlos Pires <extraterra1@gmail.com>
2024-04-04 14:24:10 +0100

    dev: eslint prettier config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,18 +3,13 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended'
-  ],
+  extends: ['standard', 'standard-jsx', 'plugin:react/recommended', 'prettier'],
   overrides: [
     {
       env: {
         node: true
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script'
       }
@@ -24,9 +19,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+  plugins: ['react'],
+  rules: {}
+};
