9db4b9423582efe918109042d71a1a97e18d2281
Carlos Pires <extraterra1@gmail.com>
2024-04-08 15:45:07 +0100

    feat: landing cta

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -3,7 +3,13 @@ import styled from 'styled-components';
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>hey</h1>
+      <div className="logo-container"></div>
+      <div className="actions-container">
+        <div className="cta">
+          <h1>Happening Now</h1>
+          <h2>Join today.</h2>
+        </div>
+      </div>
     </Wrapper>
   );
 };
@@ -14,6 +20,24 @@ const Wrapper = styled.main`
   display: grid;
   grid-template-columns: 1fr 1fr;
 
+  align-items: center;
+
   background-color: var(--black);
   color: var(--light);
+
+  & > .actions-container {
+    & .cta {
+      font-family: 'ChirpExtended';
+      font-weight: 900;
+      line-height: 13rem;
+
+      & > h1 {
+        font-size: 6rem;
+      }
+
+      & > h2 {
+        font-size: 3rem;
+      }
+    }
+  }
 `;
