8ee90b822c2791ff1e197205815356716b8d0784
Carlos Pires <extraterra1@gmail.com>
2024-04-08 15:27:45 +0100

    dev: react auth kit init"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import createStore from 'react-auth-kit/createStore';
 import './index.css';
 
+import App from './App.jsx';
 import Router from './Router.jsx';
+
+const store = createStore({
+  authName: '_auth',
+  authType: 'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:'
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
