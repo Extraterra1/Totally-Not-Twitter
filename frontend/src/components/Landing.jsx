2a1c472c80f75201da5bd023213300f10bc65e32
Carlos Pires <extraterra1@gmail.com>
2024-04-13 12:10:26 +0100

    refactor: Actions as separate component

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 import Modal from './Modal';
 import Actions from './Actions';
+import SmallPrint from './SmallPrint';
 
 const Landing = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -20,26 +21,7 @@ const Landing = () => {
           <img src={TNTLogo} alt="TNT Logo" />
         </div>
         <Actions openModal={openModal} />
-        <div className="small-print">
-          <span>About</span>
-          <span>Download the TNT App</span>
-          <span>Help Center</span>
-          <span>Terms of Service</span>
-          <span>Privacy Policy</span>
-          <span>Cookie Policy</span>
-          <span>Accesibility</span>
-          <span>Ads Info</span>
-          <span>Blog</span>
-          <span>Careers</span>
-          <span>Brand Resources</span>
-          <span>Advertising</span>
-          <span>Marketing</span>
-          <span>TNT for Business</span>
-          <span>Developers</span>
-          <span>Directory</span>
-          <span>Settings</span>
-          <span>© 2024 TNT Corp.</span>
-        </div>
+        <SmallPrint />
       </Wrapper>
     </>
   );
@@ -66,103 +48,6 @@ const Wrapper = styled.main`
       max-width: 30vw;
     }
   }
-
-  & > .actions-container {
-    & .cta {
-      font-family: 'ChirpExtended';
-      font-weight: 900;
-      line-height: 13rem;
-
-      & > h1 {
-        font-size: 6rem;
-      }
-
-      & > h2 {
-        font-size: 3rem;
-      }
-    }
-
-    & .actions {
-      display: flex;
-      flex-direction: column;
-      align-items: start;
-
-      & .buttons {
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-      }
-
-      & .separator {
-        display: grid;
-        grid-template-columns: 1fr auto 1fr;
-        align-items: center;
-        gap: 2rem;
-
-        align-self: stretch;
-        font-size: 1.5rem;
-      }
-
-      & .sign-in {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-
-        & span {
-          font-size: 1.3rem;
-          font-weight: 700;
-        }
-      }
-    }
-  }
-
-  & > .small-print {
-    display: flex;
-    grid-column: span 2;
-
-    gap: 1.5rem;
-    justify-content: center;
-    padding: 1rem;
-    color: #71767b;
-
-    & > span:not(:last-child):hover {
-      cursor: pointer;
-      text-decoration: underline;
-      text-underline-offset: 0.2rem;
-    }
-  }
-`;
-
-const Button = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-grow: 0;
-
-  gap: 1rem;
-
-  padding: 1rem 5rem;
-  background-color: ${(props) => (props.$primary ? 'var(--twitter-blue)' : props.$negative ? 'var(--black)' : 'var(--white)')};
-  font-size: 1.5rem;
-  font-weight: 700;
-  color: ${(props) => (props.$primary ? 'var(--light)' : props.$negative ? 'var(--twitter-blue)' : 'var(--black)')};
-  border-radius: 3rem;
-  cursor: pointer;
-  transition: all 0.3s;
-  border: ${(props) => (props.$negative ? '1px solid var(--gray)' : null)};
-
-  & > .btn-icon {
-    font-size: 3rem;
-  }
-
-  &:hover {
-    background-color: ${(props) => (props.$primary ? 'var(--twitter-blue-hover)' : props.$negative ? 'rgba(29,155,240,0.09)' : '#eaeaea')};
-  }
-`;
-
-const Line = styled.div`
-  height: 1px;
-  background-color: var(--gray);
 `;
 
 // TODOS
---
+++ frontend/src/components/SmallPrint.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const SmallPrint = () => {
+  return (
+    <Container>
+      <span>About</span>
+      <span>Download the TNT App</span>
+      <span>Help Center</span>
+      <span>Terms of Service</span>
+      <span>Privacy Policy</span>
+      <span>Cookie Policy</span>
+      <span>Accesibility</span>
+      <span>Ads Info</span>
+      <span>Blog</span>
+      <span>Careers</span>
+      <span>Brand Resources</span>
+      <span>Advertising</span>
+      <span>Marketing</span>
+      <span>TNT for Business</span>
+      <span>Developers</span>
+      <span>Directory</span>
+      <span>Settings</span>
+      <span>© 2024 TNT Corp.</span>
+    </Container>
+  );
+};
+
+export default SmallPrint;
+
+const Container = styled.div`
+  display: flex;
+  grid-column: span 2;
+
+  gap: 1.5rem;
+  justify-content: center;
+  padding: 1rem;
+  color: #71767b;
+
+  & > span:not(:last-child):hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-underline-offset: 0.2rem;
+  }
+`;
