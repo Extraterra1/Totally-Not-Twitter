bd27e01593fd51c04ea36c5e61d24df6b03df15e
Carlos Pires <extraterra1@gmail.com>
2024-04-22 11:49:00 +0100

    feat: sidebar buttons

--- frontend/src/views/Timeline.jsx
+++ frontend/src/views/Timeline.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { Icon } from '@iconify/react/dist/iconify.js';
+import { Link } from 'react-router-dom';
 
 import TTNLogo from '../assets/ttn-logo.png';
+import { Button as BaseButton } from '../components/Actions';
 
 const Timeline = () => {
   return (
@@ -9,10 +11,30 @@ const Timeline = () => {
       <Wrapper>
         <SidebarContainer>
           <div className="sidebar">
-            <div className="logo">
-              <img src={TTNLogo} alt="TTN Logo" />
-            </div>
-            <div className="item"></div>
+            <Link to="/timeline">
+              <div className="logo">
+                <img src={TTNLogo} alt="TTN Logo" />
+              </div>
+            </Link>
+            <Link to="/timeline">
+              <div className="item">
+                <Icon icon="ph-house-fill" />
+                <span>Home</span>
+              </div>
+            </Link>
+            <Link to="/explore">
+              <div className="item">
+                <Icon icon="ph:magnifying-glass-bold" />
+                <span>Explore</span>
+              </div>
+            </Link>
+            <Link to="/profile">
+              <div className="item">
+                <Icon icon="ph:user-fill" />
+                <span>Profile</span>
+              </div>
+            </Link>
+            <Button $primary>Post</Button>
           </div>
         </SidebarContainer>
       </Wrapper>
@@ -40,16 +62,43 @@ const SidebarContainer = styled.div`
     flex-direction: column;
     gap: 1rem;
     align-items: start;
-    min-width: 30rem;
+    min-width: 20rem;
 
     color: var(--light);
 
-    & > .logo {
-      max-width: 10rem;
+    & .logo {
+      padding: 1rem;
+      max-width: 6rem;
 
       & > img {
         max-width: inherit;
       }
     }
+
+    & .item {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      font-size: 2rem;
+      transition: all 0.3s;
+      padding: 1rem;
+      border-radius: 5rem;
+      cursor: pointer;
+
+      & > span {
+        font-weight: 700;
+      }
+
+      &:hover {
+        background-color: rgba(231, 233, 234, 0.1);
+      }
+    }
   }
+`;
+
+const Button = styled(BaseButton)`
+  /* background-color: var(--twitter-blue);
+  color: var(--white); */
+  align-self: stretch;
 `;
