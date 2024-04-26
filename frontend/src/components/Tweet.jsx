bb77776534dfaf5b47b99df0ea4dac84b8954c4e
Carlos Pires <extraterra1@gmail.com>
2024-04-26 12:01:35 +0100

    wip: profile pic in tweet

--- frontend/src/components/Tweet.jsx
+++ frontend/src/components/Tweet.jsx
@@ -1,9 +1,39 @@
+import styled from 'styled-components';
+
+import defaultPP from '../assets/profilePic.jpg';
+
 const Tweet = ({ tweet }) => {
   return (
     <>
-      <h1>{tweet.content}</h1>
+      <Container>
+        <div className="profile-pic">
+          <img src={tweet.author.profilePic || defaultPP} alt={`${tweet.author.displayName} Profile Picture`} />
+        </div>
+        <div className="content">{tweet.content}</div>
+      </Container>
     </>
   );
 };
 
 export default Tweet;
+
+const Container = styled.div`
+  border-bottom: 1px solid var(--gray-dark);
+  padding: 1rem;
+
+  display: grid;
+  grid-template-columns: auto 1fr;
+
+  & > .profile-pic {
+    display: flex;
+    align-items: start;
+    max-width: 3rem;
+    overflow: hidden;
+
+    & img {
+      border-radius: 50%;
+      max-width: inherit;
+      object-fit: contain;
+    }
+  }
+`;
