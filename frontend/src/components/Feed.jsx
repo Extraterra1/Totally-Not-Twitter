8076b5b995747559dd1223a5a1b1cf8211625509
Carlos Pires <extraterra1@gmail.com>
2024-04-26 11:47:28 +0100

    wip: fetch tweets

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,14 +1,37 @@
 import styled from 'styled-components';
 import { Toaster } from 'react-hot-toast';
+import useAxios from 'axios-hooks';
+import { useState, useEffect } from 'react';
+import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
+import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 
 import TweetForm from './TweetForm';
 
 const Feed = () => {
+  const authHeader = useAuthHeader();
+  const auth = useAuthUser();
+
+  const [offset, setOffset] = useState(0);
+  const [tweets, setTweets] = useState([]);
+  const [{ loading, data }, refreshTweets] = useAxios({
+    url: import.meta.env.VITE_API_URL + `/users/${auth._id}/timeline`,
+    method: 'GET',
+    data: { offset },
+    headers: { Authorization: authHeader }
+  });
+
+  useEffect(() => {
+    if (data?.tweets) setTweets(data?.tweets);
+  }, [data]);
+
   return (
     <>
       <Toaster position="top center" />
       <Wrapper>
         <TweetForm />
+        {tweets.map((e) => (
+          <h1 key={e._id}>{e.content}</h1>
+        ))}
       </Wrapper>
     </>
   );
