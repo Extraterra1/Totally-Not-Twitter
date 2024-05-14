983c809cf464d725791127bb3aa4f40689ebf479
Carlos Pires <extraterra1@gmail.com>
2024-05-14 15:27:15 +0100

    wip: search box styling

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Icon } from '@iconify/react/dist/iconify.js';
 import { useState } from 'react';
 
 const Discover = () => {
@@ -10,6 +11,7 @@ const Discover = () => {
     <>
       <Wrapper>
         <div className="search-box">
+          <Icon className="search-icon" icon="ph:magnifying-glass" />
           <input type="text" name="search" value={search} placeholder="Search" />
         </div>
       </Wrapper>
@@ -26,5 +28,33 @@ const Wrapper = styled.div`
 
   & > .search-box {
     margin-top: 1rem;
+    min-width: 20rem;
+    padding: 1rem 2rem;
+    border-radius: 2rem;
+
+    background-color: var(--gray-dark);
+    position: relative;
+
+    & > .search-icon {
+      position: absolute;
+      left: 1rem;
+      color: var(--gray-light);
+      font-size: 1.5rem;
+
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    & > input {
+      width: 100%;
+      outline: none;
+      border: none;
+      background-color: inherit;
+      margin-left: 1rem;
+
+      &::placeholder {
+        color: var(--gray-light);
+      }
+    }
   }
 `;
