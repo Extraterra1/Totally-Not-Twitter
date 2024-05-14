c593cead230312feeed97be681799fcbc58c1022
Carlos Pires <extraterra1@gmail.com>
2024-05-14 15:20:21 +0100

    wip: search input state

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const Discover = () => {
+  const [search, setSearch] = useState(null);
+
+  const handleChange = (e) => setSearch(e.target.value);
+
   return (
     <>
       <Wrapper>
-        <div className="search-box"></div>
+        <div className="search-box">
+          <input type="text" name="search" value={search} placeholder="Search" />
+        </div>
       </Wrapper>
     </>
   );
@@ -16,4 +23,8 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: start;
+
+  & > .search-box {
+    margin-top: 1rem;
+  }
 `;
