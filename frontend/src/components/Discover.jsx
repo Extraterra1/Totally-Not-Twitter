4b3e42d05d8bf4554504a5760df774a7358f3e3c
Carlos Pires <extraterra1@gmail.com>
2024-05-14 15:17:33 +0100

    wip: discover layout

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -3,9 +3,17 @@ import styled from 'styled-components';
 const Discover = () => {
   return (
     <>
-      <div></div>
+      <Wrapper>
+        <div className="search-box"></div>
+      </Wrapper>
     </>
   );
 };
 
 export default Discover;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+`;
