e1c1af1068230ef04dde35cd1dfaf7fb8fd23f44
Carlos Pires <extraterra1@gmail.com>
2024-05-15 15:32:13 +0100

    wip: fetch recommended follows

--- frontend/src/WhoToFollow.jsx
+++ frontend/src/WhoToFollow.jsx
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
+import useAxios from 'axios-hooks';
+import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
+import { ClipLoader } from 'react-spinners';
 
 const WhoToFollow = () => {
+  const authHeader = useAuthHeader();
+  const [{ loading, data }] = useAxios({ method: 'GET', url: `${import.meta.env.VITE_API_URL}/users/`, headers: { Authorization: authHeader } });
+  console.log(data);
+
   return (
     <Container>
       <h4 className="title">Who to follow</h4>
+      <div className="users">
+        <ClipLoader className="spinner" loading={loading} size={30} color="var(--twitter-blue)" />
+        {!loading && data.map((e) => <span key={e._id}>{e.displayName}</span>)}
+      </div>
     </Container>
   );
 };
@@ -19,5 +30,15 @@ const Container = styled.div`
   & > h4.title {
     color: var(--light);
     font-size: 1.8rem;
+  }
+
+  & > .users {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+
+    & > .spinner {
+      align-self: center;
+    }
   }
 `;
