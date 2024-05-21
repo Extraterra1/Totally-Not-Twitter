aa240c89e1804bff1ff0c717856e503490601199
Carlos Pires <extraterra1@gmail.com>
2024-05-21 12:59:31 +0100

    feat: loading spinner

--- frontend/src/components/ProfileFeed.jsx
+++ frontend/src/components/ProfileFeed.jsx
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
 import useAxios from 'axios-hooks';
 import { useParams } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners';
 
 const ProfileFeed = () => {
   const { username } = useParams();
   const [{ data, loading, error }] = useAxios({ url: `${import.meta.env.VITE_API_URL}/users/${username}`, method: 'GET' });
+
+  if (loading)
+    return (
+      <Wrapper>
+        <ClipLoader className="spinner" loading={loading} color="var(--twitter-blue)" size={45} />
+      </Wrapper>
+    );
+
+  return (
+    <Wrapper>
+      <h1>{data.user.username}</h1>
+    </Wrapper>
+  );
 };
 
 export default ProfileFeed;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > .spinner {
+    align-self: center;
+    margin-top: 3rem;
+  }
+`;
