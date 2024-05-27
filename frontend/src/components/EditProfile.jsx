483776b761349eeb1b579611e7b5e8e29bb2d286
Carlos Pires <extraterra1@gmail.com>
2024-05-27 14:48:01 +0100

    wip: modal close buttonm

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -1,15 +1,43 @@
 import Modal from './Modal';
 import styled from 'styled-components';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const EditProfile = ({ setIsOpen, isOpen }) => {
+  const closeModal = () => setIsOpen(false);
+
   return (
     <Modal setIsOpen={setIsOpen} style={modalStyles} isOpen={isOpen}>
-      <h1>hey</h1>
+      <Wrapper>
+        <Icon onClick={closeModal} className="close-icon" icon="ph:x-bold" />
+      </Wrapper>
     </Modal>
   );
 };
 
 export default EditProfile;
+
+const Wrapper = styled.div`
+  background-color: var(--black);
+  min-width: 30vw;
+  border-radius: 1.5rem;
+  padding: 2rem 1rem;
+
+  position: relative;
+
+  & > .close-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: var(--gray-light);
+    position: absolute;
+    top: 0.75rem;
+    left: 0.75rem;
+
+    &:hover {
+      color: var(--gray);
+      transition: all 0.1s;
+    }
+  }
+`;
 
 const modalStyles = {
   content: {
