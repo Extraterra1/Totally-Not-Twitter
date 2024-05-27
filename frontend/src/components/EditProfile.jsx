9455654846666b1500a7b264b0712f938b5fc5aa
Carlos Pires <extraterra1@gmail.com>
2024-05-27 14:56:14 +0100

    wip: edit form

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -1,14 +1,84 @@
 import Modal from './Modal';
 import styled from 'styled-components';
 import { Icon } from '@iconify/react/dist/iconify.js';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import useAxios from 'axios-hooks';
+import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
+import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
+import { useState } from 'react';
+
+import { Input, FileInput, SubmitButton } from './TweetForm';
+import defaultPP from '../assets/profilePic.jpg';
 
 const EditProfile = ({ setIsOpen, isOpen }) => {
+  const auth = useAuthUser();
+  const authHeader = useAuthHeader();
+  const [isFinished, setIsFinished] = useState(false);
+
   const closeModal = () => setIsOpen(false);
+
+  const [{ loading }, updateUser] = useAxios(
+    {
+      url: `${import.meta.env.VITE_API_URL}/users/${auth._id}`,
+      method: 'PATCH',
+      headers: { Authorization: authHeader }
+    },
+    { manual: true }
+  );
 
   return (
     <Modal setIsOpen={setIsOpen} style={modalStyles} isOpen={isOpen}>
       <Wrapper>
         <Icon onClick={closeModal} className="close-icon" icon="ph:x-bold" />
+        <Formik
+          initialValues={{
+            displayName: '',
+            password: '',
+            newPassword: '',
+            confirmPassword: ''
+          }}
+          validationSchema={Yup.object({
+            displayName: Yup.string().max(25, 'Must be less than 25 chars'),
+            file: Yup.mixed()
+              .test('fileType', 'Bad Image Format', (value) => {
+                if (value && value[0]) {
+                  return value[0].type === 'image/jpeg' || value[0].type === 'image/jpg' || value[0].type === 'image/png';
+                } else {
+                  return true;
+                }
+              })
+              .test('fileSize', 'Image Too Big', (value) => {
+                if (value && value[0]) {
+                  // 800 kb
+                  return value[0].size < 800 * 1000;
+                } else {
+                  return true;
+                }
+              })
+          })}
+          onSubmit={() => console.log('submit')}
+        >
+          <Form className="user-form">
+            <div className="user-field">
+              <div className="profile-pic">
+                <img src={auth?.profilePic || defaultPP} alt="Profile Picture" />
+              </div>
+              <FileInput name="file" id="file" />
+            </div>
+            <div className="buttons">
+              <SubmitButton $primary type="submit">
+                {loading ? (
+                  <BeatLoader loading={loading} size={5} color="var(--light)" />
+                ) : isFinished ? (
+                  <Icon className="done-icon" icon="ph:check-bold" />
+                ) : (
+                  'Save'
+                )}
+              </SubmitButton>
+            </div>
+          </Form>
+        </Formik>
       </Wrapper>
     </Modal>
   );
