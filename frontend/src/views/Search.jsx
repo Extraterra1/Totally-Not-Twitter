a430722ee626ee136ea624765c9314bb75e0c111
Carlos Pires <extraterra1@gmail.com>
2024-05-28 12:25:06 +0100

    wip: initial search

---
+++ frontend/src/views/Search.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Toaster } from 'react-hot-toast';
+
+import PopUpTweetForm from '../components/PopUpTweetForm';
+import { useGlobal } from '../Router';
+
+import Navbar from '../components/Navbar';
+import Discover from '../components/Discover';
+import ProfileFeed from '../components/ProfileFeed';
+
+const Search = () => {
+  return <h1>hey</h1>;
+};
+
+export default Search;
