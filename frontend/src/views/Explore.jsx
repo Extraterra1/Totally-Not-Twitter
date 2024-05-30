443303274e21ea09b812c6801704d5798771e09d
Carlos Pires <extraterra1@gmail.com>
2024-05-30 10:54:25 +0100

    wip: explore layout

--- frontend/src/views/Explore.jsx
+++ frontend/src/views/Explore.jsx
@@ -1,5 +1,43 @@
+import styled from 'styled-components';
+import { Toaster } from 'react-hot-toast';
+
+import PopUpTweetForm from '../components/PopUpTweetForm';
+import { useGlobal } from '../Router';
+
+import Navbar from '../components/Navbar';
+import Discover from '../components/Discover';
+
 const Explore = () => {
-  return <div></div>;
+  const { replyTo, modalIsOpen, setModalIsOpen } = useGlobal();
+  return (
+    <Wrapper>
+      <Navbar />
+      <div></div>
+      <Discover search={false} />
+      <PopUpTweetForm replyTo={replyTo} setIsOpen={setModalIsOpen} isOpen={modalIsOpen} update={false} />
+      <Toaster toastOptions={toastOptions} />
+    </Wrapper>
+  );
 };
 
 export default Explore;
+
+const Wrapper = styled.main`
+  background-color: var(--black);
+
+  display: grid;
+  grid-template-columns: 5fr 4fr 5fr;
+
+  gap: 3rem;
+
+  & .error-toast {
+    background-color: var(--danger-dark);
+    border-radius: 2rem;
+    color: var(--light);
+    font-size: 1.6rem;
+  }
+`;
+
+const toastOptions = {
+  success: { style: { background: 'var(--twitter-blue)', color: 'var(--light)', fontSize: '1.5rem', fontWeight: 400 }, position: 'bottom-center' }
+};
