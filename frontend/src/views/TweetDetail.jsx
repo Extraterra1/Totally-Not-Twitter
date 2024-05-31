6395e0f8e65650af694f2bcc403159357c3d3495
Carlos Pires <extraterra1@gmail.com>
2024-05-31 12:24:34 +0100

    wip: tweetdetail layout

--- frontend/src/views/TweetDetail.jsx
+++ frontend/src/views/TweetDetail.jsx
@@ -1,5 +1,42 @@
+import styled from 'styled-components';
+
+import PopUpTweetForm from '../components/PopUpTweetForm';
+import { useGlobal } from '../Router';
+
+import Navbar from '../components/Navbar';
+import Discover from '../components/Discover';
+
 const TweetDetail = () => {
-  return;
+  const { replyTo, modalIsOpen, setModalIsOpen } = useGlobal();
+  return (
+    <Wrapper>
+      <Navbar />
+      <ExploreFeed />
+      <Discover search={false} />
+      <PopUpTweetForm replyTo={replyTo} setIsOpen={setModalIsOpen} isOpen={modalIsOpen} update={false} />
+      <Toaster toastOptions={toastOptions} />
+    </Wrapper>
+  );
 };
 
 export default TweetDetail;
+
+const Wrapper = styled.main`
+  background-color: var(--black);
+
+  display: grid;
+  grid-template-columns: 5fr 4fr 5fr;
+
+  gap: 3rem;
+
+  & .error-toast {
+    background-color: var(--danger-dark);
+    border-radius: 2rem;
+    color: var(--light);
+    font-size: 1.6rem;
+  }
+`;
+
+const toastOptions = {
+  success: { style: { background: 'var(--twitter-blue)', color: 'var(--light)', fontSize: '1.5rem', fontWeight: 400 }, position: 'bottom-center' }
+};
