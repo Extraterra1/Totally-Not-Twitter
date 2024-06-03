b53aeee23a6256981e66c2c0d038ce8788b41d68
Carlos Pires <extraterra1@gmail.com>
2024-06-03 15:15:58 +0100

    feat: responsive profile

--- frontend/src/views/Timeline.jsx
+++ frontend/src/views/Timeline.jsx
@@ -66,7 +66,7 @@ const Wrapper = styled.main`
 
   @media screen and (max-width: 500px) {
     grid-template-columns: 1fr;
-    grid-template-rows: 1fr auto;
+    grid-template-rows: auto 1fr;
     gap: 0;
   }
 `;
