3a3fcccbb006c8998ca88770c12aa735154a197d
Carlos Pires <extraterra1@gmail.com>
2024-06-06 11:34:09 +0100

    fix: search box border

--- frontend/src/components/SearchFeed.jsx
+++ frontend/src/components/SearchFeed.jsx
@@ -94,6 +94,10 @@ const Wrapper = styled.div`
   border-left: 1px solid var(--gray-dark);
   border-right: 1px solid var(--gray-dark);
 
+  @media screen and (max-width: 500px) {
+    border: none;
+  }
+
   & .spinner {
     align-self: center;
     margin-top: 3rem;
