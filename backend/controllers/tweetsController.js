542c62881a9b3acd0fa8084115c0ee8947c583e2
Carlos Pires <extraterra1@gmail.com>
2024-06-08 09:42:07 +0100

    dev: clog cleanup

--- backend/controllers/tweetsController.js
+++ backend/controllers/tweetsController.js
@@ -63,9 +63,6 @@ exports.createTweet = [
         })
       : null;
 
-    console.log(req.file);
-    console.log(image);
-
     const { content, tweetType, replyTo, retweetedTweet } = req.body;
     const author = req.user._id;
 
